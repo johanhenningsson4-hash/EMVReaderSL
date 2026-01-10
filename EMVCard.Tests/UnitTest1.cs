d028eda16fbb1dc08b4865c62825b06e3be74c45
Johan Henningsson <johan129971@hotmail.com>
2026-01-10 18:35:48 +0100

    Added FluentAssertions

--- EMVCard.Tests/UnitTest1.cs
+++ EMVCard.Tests/UnitTest1.cs
@@ -1,10 +1,13 @@
-﻿namespace EMVCard.Tests;
+﻿using Xunit;
 
-public class UnitTest1
+namespace EMVCard.Tests
 {
-    [Fact]
-    public void Test1()
+    public class UnitTest1
     {
+        [Fact]
+        public void Test1()
+        {
 
+        }
     }
 }
